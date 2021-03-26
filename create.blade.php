82e8f1e39b06eb1e6290b90bdd58f23dc8f5490e
Ruel Saludaga <panyero2003@yahoo.com>
2021-03-26 10:44:56 +0800

    Complete Ecommerce

---
+++ create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>eCommerce Sandra</title>
+  <link href="{{asset('root/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css')}}">
+  <link href="{{asset('root/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('root/css/ruang-admin.min.css')}}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  @include('notify::messages')
+       <x:notify-messages />
+        @notifyJs
